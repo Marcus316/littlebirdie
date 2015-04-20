5f41281ebb66b69c7f45f9cf1aa02d79ff693784
Marcus316 <mreside@gmail.com>
2015-04-19 21:07:13 -0400

    Create conf.lua

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+	t.title = "LD32" -- The title of the window the game is in (string)
+	t.version = "0.9.2"         -- The LÖVE version this game was made for (string)
+	t.window.width = 800
+	t.window.height = 600
+
+	-- For Windows debugging
+	t.console = false
+end
